05fc445e4850e980d77cc49a6ce50ad59ecd3a88
venkatkukatla <104503378+venkatkukatla@users.noreply.github.com>
2024-07-07 20:57:50 +0530

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    encrypt = true    
+    bucket = "myfytrtrygddd"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
